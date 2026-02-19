dbac10b5a6ae3910cb255ebf32ade842805c21ef
Ishtiyaque Khan <ishtiyaquekhan529@gmail.com>
2026-02-19 15:51:27 +0530

    Time: 3 ms (14.05%), Space: 43.1 MB (51.23%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int splitArray(int[] arr, int k) {
+
+        int low = Arrays.stream(arr).max().getAsInt();
+        int high = Arrays.stream(arr).sum();
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (isItPossible(arr, k, mid)) {
+                ans = mid;       // store the feasible answer
+                high = mid - 1;  // try to minimize
+            } else {
+                low = mid + 1;   // increase limit
+            }
+        }
+        return ans;
+    }
+
+    public boolean isItPossible(int[] arr, int k, int mid) {
+        int studentCount = 1;
+        int pages = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (pages + arr[i] <= mid) {
+                pages += arr[i];
+            } else {
+                studentCount++;
+                pages = arr[i];
+            }
+            if (studentCount > k) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
